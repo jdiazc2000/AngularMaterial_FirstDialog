99808eb195c6ed55fea0a56d8edbfbf8435ffaec
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-06-06 15:51:43 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DialogComponent } from './components/dialog/dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
-  ],
+    MatDialogModule,
+],
+  entryComponents: [DialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
